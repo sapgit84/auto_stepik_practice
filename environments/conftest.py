a6299e944d270538fa36c081ec3d0d1d70dcee6c
Andrey <shibanov@llcglobaladvisors.com>
2023-03-21 22:43:20 +0300

    deleted comment

--- environments/conftest.py
+++ environments/conftest.py
@@ -1,21 +1,16 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 
-def pytest_addoption(parser):
-    # parser.addoption('--browser_name', action='store', default=None,
-    # help="Choose browser: '--browser_name=chrome' or '--browser_name=firefox'")
 
+def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="Choose language: '--language=en' or '--language=ru'")
-
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
     browser = webdriver.Chrome(options=options)
@@ -23,3 +18,5 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
+
+
--- environments/test_items.py
+++ environments/test_items.py
@@ -11,12 +11,10 @@ try:
     def test_item_page_have_submit_button(browser):
         browser.implicitly_wait(5)
         browser.get(link)
-        # find Submit button
         submit_button = browser.find_elements(By.XPATH, "//button[@type='submit'][@class='btn btn-lg btn-primary "
                                                         "btn-add-to-basket']")
         assert len(submit_button) > 0, 'button not found'
         time.sleep(4)
-
 
 finally:
     time.sleep(5)
